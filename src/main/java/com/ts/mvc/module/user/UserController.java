4cef4b333c2eb242e5238aed6377869bd2a20cda
jeje-1 <econoone@gmail.com>
2023-03-09 18:01:17 +0900

    usercontroller

--- src/main/java/com/ts/mvc/module/user/UserController.java
+++ src/main/java/com/ts/mvc/module/user/UserController.java
@@ -1,6 +1,5 @@
 package com.ts.mvc.module.user;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +10,19 @@ import lombok.AllArgsConstructor;
 @RequestMapping("user")
 @AllArgsConstructor
 public class UserController {
-	
-	@GetMapping("signup")
-	public String signup() {
-		System.out.println("ldm");
-		return "/html/signup";
-	}
+   
+   @GetMapping("signup")
+   public String signUp() {
+      return "/html/signup";
+   }
 
-	@GetMapping("login")
-	public String login() {
-		return "/html/login";
-	}
-	
-	@GetMapping("modify")
-	public String userModify() {
-		return "/html/user-modify";
-	}
+   @GetMapping("login")
+   public String login() {
+      return "/html/login";
+   }
+   
+   @GetMapping("modify")
+   public String userModify() {
+      return "/html/user-modify";
+   }
 }
