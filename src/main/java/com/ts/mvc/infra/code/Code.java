8577fd6dea8090e8e5786a96fe7771302e7a7780
jeje-1 <econoone@gmail.com>
2023-03-10 16:24:22 +0900

    openai api 설계중

--- src/main/java/com/ts/mvc/infra/code/Code.java
+++ src/main/java/com/ts/mvc/infra/code/Code.java
@@ -3,7 +3,8 @@ package com.ts.mvc.infra.code;
 public enum Code {
 	
 	DOMAIN("http://localhost:8080"),
-	SMTP_FROM("eedomeng@naver.com");
+	SMTP_FROM("eedomeng@naver.com"),
+	OPENAI_API_KEY("");
 	
 	// 배포시 servlet-context.xml의 resources 경로도 함께 수정
 //	STORAGE_PATH("C:\\Program Files\\CODE\\storage\\");
