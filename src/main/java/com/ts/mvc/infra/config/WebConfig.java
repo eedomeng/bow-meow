9eb3ce081da958a936877dcacc847b5ab231ee49
eedomeng <eedomeng@gmail.com>
2023-03-31 16:02:20 +0900

    dm

--- src/main/java/com/ts/mvc/infra/config/WebConfig.java
+++ src/main/java/com/ts/mvc/infra/config/WebConfig.java
@@ -1,13 +1,28 @@
 package com.ts.mvc.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	@Value("${file.path}")
+	private String uploadFolder;
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		registry.addResourceHandler("/upload/**") // /upload/** 패턴이 나오면 발동
+				.addResourceLocations("file:///" + uploadFolder)
+				.setCachePeriod(60*10*6) // 1시간 동안
+				.resourceChain(true)
+				.addResolver(new PathResourceResolver());
+	}
 	
 }
