89b58e7e58cf8ba8c79731572f9d0eb99be0873c
eedomeng <eedomeng@gmail.com>
2023-04-04 15:39:36 +0900

    diary dm

--- src/main/java/com/ts/mvc/module/diary/Diary.java
+++ src/main/java/com/ts/mvc/module/diary/Diary.java
@@ -20,12 +20,13 @@ import com.ts.mvc.module.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @DynamicInsert // insert 쿼리를 생성할 때 null인 필드는 쿼리에서 생략
 @DynamicUpdate // entity에서 변경이 발견되지 않은 값은 쿼리에서 생략
-@Builder @NoArgsConstructor @AllArgsConstructor @Data
+@Builder @NoArgsConstructor @AllArgsConstructor @Getter
 public class Diary {
 	
 	@Id
