7ac45acf1515bdfd9f5c86f05d170b26027418de
CharmingGroot <ohyes9711@gmail.com>
2023-04-05 14:38:47 +0900

    chg

--- src/main/java/com/ts/mvc/module/guestbook/GuestBookService.java
+++ src/main/java/com/ts/mvc/module/guestbook/GuestBookService.java
@@ -32,147 +32,144 @@ import lombok.AllArgsConstructor;
 @Transactional(readOnly = true)
 @AllArgsConstructor
 public class GuestBookService {
-	
-	private final UserRepository userRepository;
-	private final GuestBookRepository guestBookRepository;
+   
+   private final UserRepository userRepository;
+   private final GuestBookRepository guestBookRepository;
 
-	@Transactional
-	public void createGuestBook(GuestBookRegistRequest dto) {
-		User user = userRepository.findById(dto.getUserId()).get();
-		GuestBook guestBook = GuestBook.createGuestBook(dto, user);
-		
-		guestBookRepository.saveAndFlush(guestBook);
-		
-	}
-	
-	
-	// 수정기능
-	// 매개변수는 dto가 되는 것이 맞음.
-	@Transactional
-	public void updateGuestBook(GuestBookUpdateRequest dto) {
-		
-		// 칼럼조회 dto가 받아온 gbIdx와 일치하는 엔티티를 조회
-		Optional<GuestBook> guestbook = guestBookRepository.findById(dto.getGbIdx());
-		
-		GuestBook guestBook = guestbook.get();
-		
-		guestBook.setContent(dto.getContent());
-		
-		
-		// save n flush
-		guestBookRepository.saveAndFlush(guestBook);
-		
-				
-		
-		System.out.println("updateGuestBook의 결과값"+ guestBook);
-//		System.out.println("guestBook.getUser의 결과값" +guestBook.getUser()); // set 안해줘서 null
-//		System.out.println("guestBook.getGbIdx의 결과값" +guestBook.getGbIdx());
-		System.out.println("updateGuestBook Service 실행");
-		
-	}
+   @Transactional
+   public void createGuestBook(GuestBookRegistRequest dto) {
+      User user = userRepository.findById(dto.getUserId()).get(); // 방문자의 id로 조회
+      GuestBook guestBook = GuestBook.createGuestBook(dto, user);
+      
+      guestBookRepository.saveAndFlush(guestBook);
+      
+   }
+   
+   
+   // 수정기능
+   // 매개변수는 dto가 되는 것이 맞음.
+   @Transactional
+   public void updateGuestBook(GuestBookUpdateRequest dto) {
+      
+      // 칼럼조회 dto가 받아온 gbIdx와 일치하는 엔티티를 조회
+      Optional<GuestBook> guestbook = guestBookRepository.findById(dto.getGbIdx());
+      
+      GuestBook guestBook = guestbook.get();
+      
+      guestBook.setContent(dto.getContent());
+      
+      
+      // save n flush
+      guestBookRepository.saveAndFlush(guestBook);
+      
+            
+      
+      System.out.println("updateGuestBook의 결과값"+ guestBook);
+//      System.out.println("guestBook.getUser의 결과값" +guestBook.getUser()); // set 안해줘서 null
+//      System.out.println("guestBook.getGbIdx의 결과값" +guestBook.getGbIdx());
+      System.out.println("updateGuestBook Service 실행");
+      
+   }
 
-	@Transactional
-	public void removeGuestBook(Long gbIdx, Principal principal) {
-		GuestBook guestBook = guestBookRepository.findById(gbIdx).orElseThrow(() -> new HandlableException(ErrorCode.NOT_EXISTS));
-		
-		if(!guestBook.getUser().getUserId().equals(principal.getUserId())) throw new AuthException(ErrorCode.UNAUTHORIZED_REQUEST);
-		
-		guestBookRepository.delete(guestBook);
-	}
+   @Transactional
+   public void removeGuestBook(Long gbIdx, Principal principal) {
+      GuestBook guestBook = guestBookRepository.findById(gbIdx).orElseThrow(() -> new HandlableException(ErrorCode.NOT_EXISTS));
+      
+      if(!guestBook.getUser().getUserId().equals(principal.getUserId())) throw new AuthException(ErrorCode.UNAUTHORIZED_REQUEST);
+      
+      guestBookRepository.delete(guestBook);
+   }
 
-	@Transactional
-	public void deleteGuestBook(GuestBookDeleteRequest dto) {
-		try {
-			GuestBook guestBook = guestBookRepository.findById(dto.getGbIdx()).orElse(null); // 먼저 해당 Entity를 조회합니다.
-			System.out.println("Service Layer : Delete : "+guestBook);
-			
-			// 삭제할 gbIdx 행 다음 순서의 글들을 조회 >> gbIdx가 primary key로 설정되어있어서 변경이 불가함.
+   @Transactional
+   public void deleteGuestBook(GuestBookDeleteRequest dto) {
+      try {
+         GuestBook guestBook = guestBookRepository.findById(dto.getGbIdx()).orElse(null); // 먼저 해당 Entity를 조회합니다.
+         System.out.println("Service Layer : Delete : "+guestBook);
+         
+         // 삭제할 gbIdx 행 다음 순서의 글들을 조회 >> gbIdx가 primary key로 설정되어있어서 변경이 불가함.
 //            List<GuestBook> nextGuestBooks = guestBookRepository.findNextGuestBooks(dto.getGbIdx());
 
 //            for(GuestBook guestbook : nextGuestBooks) {
-//            	guestbook.setGbIdx(guestbook.getGbIdx()-1);
+//               guestbook.setGbIdx(guestbook.getGbIdx()-1);
 //            }
-			
-			
-			guestBookRepository.delete(guestBook); // 조회된 Entity를 delete 메소드에 전달합니다.			
-		} catch (Exception e) {
-			System.out.println("게시글이 존재하지 않습니다.");
-		}
-		
-		
-	}
+         
+         
+         guestBookRepository.delete(guestBook); // 조회된 Entity를 delete 메소드에 전달합니다.         
+      } catch (Exception e) {
+         System.out.println("게시글이 존재하지 않습니다.");
+      }
+      
+      
+   }
 
-	
-	
-	public Map<String, Object> findGuestBookList(Pageable pageable) {
-		Page<GuestBook> page = guestBookRepository.findAll(pageable);
-		
-		Paging paging = Paging.builder()
-							  .page(page)
-							  .blockCnt(5)
-							  .build();
-		
-		return Map.of("guestBookList", GuestBookListResponse.toDtoList(page.getContent()), "paging", paging);
-	}
+   
+   
+   public Map<String, Object> findGuestBookList(Pageable pageable) {
+      Page<GuestBook> page = guestBookRepository.findAll(pageable);
+      
+      Paging paging = Paging.builder()
+                       .page(page)
+                       .blockCnt(5)
+                       .build();
+      
+      return Map.of("guestBookList", GuestBookListResponse.toDtoList(page.getContent()), "paging", paging);
+   }
 
 
-	@Transactional(readOnly = true)
-	public PageOwnerDTO guestBookForm(String pageOwnerNickName, String visitUserId) {
+   @Transactional(readOnly = true)
+   public PageOwnerDTO guestBookForm(String pageOwnerNickName, String visitUserId) {
 
-		
-		PageOwnerDTO dto = new PageOwnerDTO();
-		
-		// 페이지의 소유여부 검증로직
-		// gbIdx가 1인 값의 userId와 principalId가 일치한다면?
-		// Y/N
-		// Y -> 모든 글 삭제권한 부여 && 작성글에 대한 수정권한 부여
-		// N -> 방문자로 간주, 작성글에 대한 수정권한과 && 삭제 권한 부여
-		
-		// 페이지 소유여부 검증로직
-		// 1. GuestBook Entity에서 1번 gbIdx에 해당하는 데이터 찾기
-	    GuestBook guestBookEntity = guestBookRepository.findByGbIdx((long) 1).orElseThrow(() -> {
-	        throw new CustomException("존재하지 않는 게스트북입니다.");
-	    });
-		
-	    
-	    // 2.로직 : gbIdx == 1인 값과 현재 방문자의 userId를 비교하여 현재 방문자가 페이지의 소유권을 가지고 있는지 판단한다.
-	    if(guestBookEntity.getUser().getUserId().equals(visitUserId)) {
-	    	dto.setUser(visitUserId);
-	    	dto.setPageOwner(true); // 페이지 소유자 O
-	    	dto.setPageVisitor(false); // 방문자 X
-	    	dto.setCanDeleteAll(true); // 모든 삭제권한 부여 O
-	    	dto.setCanDelete(false); // 작성글에 대한 삭제권한 X : 더 큰 권한이 부여되었기 때문.
-	    	dto.setCanModify(true); // 작성글에 대한 수정권한 부여
-	    	System.out.println("게스트북 페이지의 주인입니다.");
-	    }
-	    else {
-	    	dto.setUser(visitUserId);
-	    	dto.setPageVisitor(true); // 방문자 O
-	    	dto.setPageOwner(false); // 소유자 X
-	    	dto.setCanDelete(true); // 작성글 삭제권한 부여 O
-	    	dto.setCanDeleteAll(false); // 모든 삭제권한 부여 X
-	    	dto.setCanModify(true); // 작성글 수정권한 부여
-	    	System.out.println("게스트북 페이지의 방문자입니다.");
-	    }
-	    		
-		return dto;
-	}
+      
+      PageOwnerDTO dto = new PageOwnerDTO();
+      // 페이지 주인 설정
+      
+    
+      // 1. 페이지 주인의 id로 DB조회
+       User guestBookEntity = userRepository.findById(pageOwnerNickName).orElseThrow(() -> {
+         throw new CustomException("존재하지 않는 게스트북입니다.");
+      });
 
-	// 3. 권한에 따작성글 리스트 추출
+       
+       // 2 1에서 가져온 DB와 방문자 id를 비교
+       if(guestBookEntity.getUserId().equals(visitUserId)) {
+    	  dto.setOwner(pageOwnerNickName);
+          dto.setVisitor(visitUserId);
+          dto.setPageOwner(true); // 페이지 소유자 O
+          dto.setPageVisitor(false); // 방문자 X
+          dto.setCanDeleteAll(true); // 모든 삭제권한 부여 O
+          dto.setCanDelete(false); // 작성글에 대한 삭제권한 X : 더 큰 권한이 부여되었기 때문.
+          dto.setCanModify(true); // 작성글에 대한 수정권한 부여
+          System.out.println("게스트북 페이지의 주인입니다.");
+       }
+       else {
+    	  dto.setOwner(pageOwnerNickName);
+          dto.setVisitor(visitUserId);
+          dto.setPageVisitor(true); // 방문자 O
+          dto.setPageOwner(false); // 소유자 X
+          dto.setCanDelete(true); // 작성글 삭제권한 부여 O
+          dto.setCanDeleteAll(false); // 모든 삭제권한 부여 X
+          dto.setCanModify(true); // 작성글 수정권한 부여
+          System.out.println("게스트북 페이지의 방문자입니다.");
+       }
+             
+      return dto;
+   }
+
+   // 3. 권한에 따작성글 리스트 추출
 
 
 
 
 
-	
+   
 
-//	public GuestBookDetailResponse findGuestBookByGbIdx(Long gbIdx) {
-//		GuestBook guestBook = guestBookRepository.findById(gbIdx)
-//					.orElseThrow(() -> new HandlableException(ErrorCode.NOT_EXISTS));
-//		
-//		return new GuestBookDetailResponse(guestBook);
-//	}
+//   public GuestBookDetailResponse findGuestBookByGbIdx(Long gbIdx) {
+//      GuestBook guestBook = guestBookRepository.findById(gbIdx)
+//               .orElseThrow(() -> new HandlableException(ErrorCode.NOT_EXISTS));
+//      
+//      return new GuestBookDetailResponse(guestBook);
+//   }
 
-	
+   
 
 }
