fd08ed4d9abdf56b53db33acf7909040bba3e3c6
jiyul0830 <jiyul0830@gmail.com>
2023-04-06 21:54:38 +0900

    random chat hw

--- src/main/resources/static/assets/js/random-chat.js
+++ src/main/resources/static/assets/js/random-chat.js
@@ -1,22 +1,22 @@
-const chatPartners = ["DB자료 활용하기"]; // 채팅 상대 목록
+// const chatPartners = ["db목록"]; // 채팅 상대 목록
 
-const randomChatPartners = shuffleArray(chatPartners); // 무작위 상대 목록 만들기
+// const randomChatPartners = shuffleArray(chatPartners); // 무작위 상대 목록 만들기
 
-// 셔플 함수
-function shuffleArray(array) {
-  const shuffledArray = [...array];
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
-  return shuffledArray;
-}
+// // 셔플 함수
+// function shuffleArray(array) {
+//   const shuffledArray = [...array];
+//   for (let i = shuffledArray.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+//   }
+//   return shuffledArray;
+// }
 
-// 상대 목록 출력
-console.log("산책 메이트를 추천합니다"); 
-randomChatPartners.forEach(partner => {
-  console.log(partner);
-});
+// // 상대 목록 출력
+// console.log("산책 메이트를 추천합니다"); 
+// randomChatPartners.forEach(partner => {
+//   console.log(partner);
+// });
 
 async function recommendFriend() {
     try {
